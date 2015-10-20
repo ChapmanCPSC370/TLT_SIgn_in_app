b0c5bdb0691bffb8b24384cd4e6e2fc0685eeee6
samy achour <sachour96@gmail.com>
2015-10-19 23:47:05 -0700

    Completely revamped branch, now a single view controller with textfield, button, and tableView

--- TLT_SIgn_in_app/ViewController.swift
+++ TLT_SIgn_in_app/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var items = [String]()
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    @IBAction func addButton(sender: UIButton) {
+        
+        let newItem = textField.text
+        items.append(newItem!)
+        textField.resignFirstResponder()
+        
+        textField.text = ""
+        tableView.reloadData()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +37,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return items.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+        
+    }
 
 }
 
