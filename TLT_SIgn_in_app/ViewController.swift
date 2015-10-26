d0370c26327d1c5072d6c1abe47f8f1b5e555b8e
samy achour <sachour96@gmail.com>
2015-10-25 18:56:49 -0700

    Restructured multiple functions, added ability to test for Parse database update success and notify user if their internet connection (or other reason) has stopped the app from updated Parse Added comments

--- TLT_SIgn_in_app/ViewController.swift
+++ TLT_SIgn_in_app/ViewController.swift
@@ -11,51 +11,18 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // MARK: Global core variables
+    
     var personInfo: String = ""
     var items = [String]()
-    var updatedDatabase = false
+    
+    // MARK: App utilities
     
     @IBOutlet var textField: UITextField!
     
     @IBOutlet var textFieldPerson: UITextField!
     
     @IBOutlet var tableView: UITableView!
-    
-    @IBAction func addButton(sender: UIButton) {
-        
-        let newItem = textField.text
-        let newInfo = textFieldPerson.text
-        
-        personInfo = newInfo!
-        
-        let dataString = addUnderscores(newItem!)
-        
-        //if the data that the user inputted is correct
-        if (dataString != ""){
-            
-            updateDatabase(dataString, rowData: personInfo)
-            
-            if (updatedDatabase == true) {
-            
-                items.insert(newItem! + " " + getDate(), atIndex: 0)
-                textField.resignFirstResponder()
-            
-                textField.text = ""
-                tableView.reloadData()
-            
-                saveCoreVariables()
-                
-                updatedDatabase = false
-            
-            } else {
-                
-                alert("Connection", alertMessage: "Could not update database")
-                
-            }
-            
-        }
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,53 +32,68 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButton(sender: UIButton) {
+        
+        let newItem = textField.text
+        let newInfo = textFieldPerson.text
+        
+        let dataString = addUnderscores(newItem!)
+        
+        //if the user actually typed something in both text fields
+        if (dataString != "" && newInfo! != ""){
+            
+            update(dataString, rowData: newInfo!, tableRowString: newItem!)
+            
+        } else {
+            
+            alert("Invalid input", alertMessage: "Text field empty")
+            
+        }
+        
+    }
+    
+    // MARK: Updating data in Parse/Table functions
+    
+    //Updates Parse database and Table cells
+    func update(columnName: String, rowData: String, tableRowString: String) {
+        
+        updateDatabase(columnName, rowData: rowData)
+        
+        //Adds latest value to the items array which the 'History' table loads from
+        items.insert(tableRowString + " " + getDate(), atIndex: 0)
+        textField.resignFirstResponder()
+            
+        textField.text = ""
+        tableView.reloadData()
+            
+        saveCoreVariables()
+        
+    }
+    
     //update the Parse database
     func updateDatabase(columnName: String,rowData: String){
         
         let SIObject = PFObject(className: "SI_Sign_in")
         SIObject[columnName] = rowData
-        SIObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in self.updatedDatabaseTrue() }
-        
-    }
-    
-    func updatedDatabaseTrue() {
-        updatedDatabase = true
-    }
-    
-    //Create an alert with a title, message, and dismiss button
-    func alert(alertTitle: String,alertMessage: String){
-        
-        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
-        
-        alertController.addAction(UIAlertAction(title: "dismiss", style: UIAlertActionStyle.Default,handler: nil))
-        
-        self.presentViewController(alertController, animated: true, completion: nil)
-        
-    }
-    
-    //So we can convert "Bob Smith" to "bob_smith"
-    func addUnderscores(var inputString: String) -> String{
-        
-        var returnString = ""
-        
-        //Check if the string contains a date with forward slashes
-        if inputString != "" {
+        SIObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
             
-            //split string by the spaces, then join it by underscore
-            let newItemArr = inputString.componentsSeparatedByString(" ")
-            inputString = newItemArr.joinWithSeparator("_")
-            
-            returnString = inputString.lowercaseString
-            
-        } else {
-            
-            alert("Invalid entry", alertMessage: "Empty input")
+            //if database was not successfully updated
+            if !success {
+                
+                self.alert("Internet Connection", alertMessage: "Could not update database, Even though value was added to history, online database was not updated. Please try again. If this window does not appear then update was successful.")
+                
+            }
         
         }
         
-        return returnString
-        
     }
+    
+    // MARK: Core variable functions
     
     //Saves the variables we need to remember
     func saveCoreVariables(){
@@ -142,22 +124,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    //Get current date and time for table data insertion
-    func getDate() -> String {
-        
-        let todaysDate:NSDate = NSDate()
-        let dateFormatter:NSDateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
-        let DateInFormat:String = dateFormatter.stringFromDate(todaysDate)
-        return DateInFormat
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    // MARK: Table View functions
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
         return items.count
@@ -174,6 +142,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    //When the user clicks on a tableview cell the SI name in that cell is put in the SI name textfield
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         let autoCompleteString = items[indexPath.row]
@@ -182,6 +151,54 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         textField.text = returnString[0] + " " + returnString[1]
         
         tableView.reloadData()
+        
+    }
+    
+    // MARK: Tools
+    
+    //Create an alert with a title, message, and dismiss button
+    func alert(alertTitle: String,alertMessage: String){
+        
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alertController.addAction(UIAlertAction(title: "dismiss", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+    //Get current date and time for table data insertion
+    func getDate() -> String {
+        
+        let todaysDate:NSDate = NSDate()
+        let dateFormatter:NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+        let DateInFormat:String = dateFormatter.stringFromDate(todaysDate)
+        return DateInFormat
+        
+    }
+
+    //So we can convert "Bob Smith" to "bob_smith"
+    func addUnderscores(var inputString: String) -> String{
+        
+        var returnString = ""
+        
+        let newItemArr = inputString.componentsSeparatedByString(" ")
+        
+        //If there were more than two words in the SI name input
+        if newItemArr.count > 2 {
+                
+            alert("SI Name", alertMessage: "The SI name should only have one space")
+                
+        } else {
+                
+            inputString = newItemArr.joinWithSeparator("_")
+                
+            returnString = inputString.lowercaseString
+                
+        }
+        
+        return returnString
         
     }
 
